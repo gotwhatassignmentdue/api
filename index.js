b9aded1611e33128c42cbd2007e25a758924241a
Ivan Tee <e0319256@u.nus.edu>
2021-01-08 23:51:14 +0800

    update index.js

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const express = require("express");
 const app = express();
 const { authUser } = require('./verifyToken');
-const port = 3000
+const port = 3001
+const axios = require('axios').default;
+const userID = "123";
 
 // get list of tasks
 app.get("/", authUser, async (req, res) => {
@@ -23,13 +25,11 @@ app.listen(port, () => {
 
 app.get("/initiateTelebot/", (req, res) => {
     try {
-      req, res => router.get("link/new/:fakeUserID1");
+      axios
+      .post(`http://localhost:3000/link/new/:${userID}`)
+      .then((response) => res.status(200).json(response.data))
 
       // need to change to telebot APIs response
-      res.json({
-        userID : "testID",
-        link : "testLink"
-      })
     } catch (err) {
       console.log(err.message)
     }
