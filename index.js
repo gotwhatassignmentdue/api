39807ed78fcbc82836cd3113d3dfb646fdf9d8c8
Ivan Tee <e0319256@u.nus.edu>
2021-01-08 17:14:00 +0800

    update index.js

--- index.js
+++ index.js
@@ -25,7 +25,26 @@ app.get("/", async (req, res) => {
     console.log(err.message);
   }
 })
-  
-  app.listen(port, () => {
-    console.log(`Example app listening at http://localhost:${port}`)
-  })
+
+app.listen(port, () => {
+   console.log(`Example app listening at http://localhost:${port}`)
+})
+
+// API TO GET USER INFO
+
+// need router for telebot
+
+app.get("/initiateTelebot/", (req, res) => {
+    try {
+      req, res => router.get("link/new/:fakeUserID1");
+
+      // need to change to telebot APIs response
+      res.json({
+        userID : "testID",
+        link : "testLink"
+      })
+    } catch (err) {
+      console.log(err.message)
+    }
+  }
+)
