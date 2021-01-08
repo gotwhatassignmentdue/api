911b315f7685c975adf4b240d17302d49bbe2f5f
dearvae <lovevae7@gmail.com>
2021-01-08 15:22:55 +0800

    add index js

---
+++ index.js
@@ -0,0 +1,31 @@
+const express = require("express");
+const app = express();
+const port = 3000
+
+// get list of tasks
+router.get("/", async (req, res) => {
+  req.token
+  try {
+
+    
+    // const tasks = [
+    //   {
+    //     id: "string",
+    //     targetID: "string",
+    //     targetName: "string",
+    //     taskType: 0,
+    //     typeID: 0,
+    //     createdDate: "string",
+    //     startDate: "string",
+    //     endDate: "string",
+    //     completed: true,
+    //   },
+    // ];
+  } catch (err) {
+    console.log(err.message);
+  }
+})
+  
+  app.listen(port, () => {
+    console.log(`Example app listening at http://localhost:${port}`)
+  })
