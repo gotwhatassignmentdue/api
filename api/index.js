4b6043baa23eda07198b8403bd0ef945cea8322a
Jaime <jaimeloeuf@gmail.com>
2021-01-09 11:05:41 +0800

    Fix API and cleanup.

--- api/index.js
+++ api/index.js
@@ -1,56 +1,52 @@
 const express = require("express");
 const app = express();
-const { authUser } = require('./verifyToken');
-const port = 3001
-const axios = require('axios').default;
+const { authUser } = require("./verifyToken");
+const port = 3001;
+const axios = require("axios").default;
 const userID = "123";
 
 // get list of tasks
 app.get("/task", authUser, async (req, res) => {
   try {
-    data = req.userData.tasks;
-
-    data.sort(function(a,b){
-      return new Date(b.endDate) - new Date(a.endDate);
-    });
-
-    res.status(200).json(data.filter(task => task.completed === true));
+    res
+      .status(200)
+      .json(
+        req.userData.tasks.sort(
+          (a, b) => new Date(b.endDate) - new Date(a.endDate)
+        )
+      );
   } catch (err) {
     console.log(err.message);
   }
-})
+});
 
 // get list of tasks by module code
 app.get("/task/:module", authUser, async (req, res) => {
   const { module } = req.params;
   try {
     const data = req.userData.tasks;
-    res.status(200).json(data.filter(task => task.module === module));
+    res.status(200).json(data.filter((task) => task.module === module));
   } catch (err) {
     console.log(err.message);
   }
-})
-
-
+});
 
 // API TO GET USER INFO
 
 // need router for telebot
 
 app.get("/initiateTelebot/", (req, res) => {
-    try {
-      axios
+  try {
+    axios
       .post(`http://localhost:3000/link/new/:${userID}`)
-      .then((response) => res.status(200).json(response.data))
+      .then((response) => res.status(200).json(response.data));
 
-      // need to change to telebot APIs response
-    } catch (err) {
-      console.log(err.message)
-    }
+    // need to change to telebot APIs response
+  } catch (err) {
+    console.log(err.message);
   }
-)
-
+});
 
 app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`)
-})
+  console.log(`Example app listening at http://localhost:${port}`);
+});
--- api/verifyToken.js
+++ api/verifyToken.js
@@ -1,15 +1,15 @@
-const data = require('./src/mockdata.json')
+const data = require("./src/mockdata.json");
 function authUser(req, res, next) {
-    const token = req.header('auth-token');
-    if(!token) return res.status(401).send('Acess Denied');
-        if(data[token]){
-            req.userData = data[token]
-            next();
-        } else {
-            res.status(400).send('Invalid Token');
-        }   
+  const token = req.header("auth-token");
+  if (!token) return res.status(401).send("Acess Denied");
+  if (data[token]) {
+    req.userData = data[token];
+    next();
+  } else {
+    res.status(400).send("Invalid Token");
+  }
 }
 
 module.exports = {
-    authUser
-}
+  authUser,
+};
